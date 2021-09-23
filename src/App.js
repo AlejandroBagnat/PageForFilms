5c3c30ed1150e8765340551f0f3dc525f9e7e8fd
AlejandroBagnat <43849183+AlejandroBagnat@users.noreply.github.com>
2021-09-23 17:11:44 -0300

    inicializacion

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import PageWrapper from './PageWrapper';
+
+
+function App() {
+
+
+  
+ 
+
+
+
+  return (
+    <PageWrapper/>
+  );
+}
+
+export default App;
---
+++ src/Pelicula.js
@@ -0,0 +1,35 @@
+export default function Pelicula(props) {
+
+
+    
+  return (
+    <div className="movie-item-style-2">
+      <img src={props.imagen} alt="" />
+      <div class="mv-item-infor">
+        <h6>
+          <a href="moviesingle.html">
+            {props.titulo}
+          </a>
+        </h6>
+        <p class="rate">
+          <i class="ion-android-star"></i>
+          <span>{props.calificacion}</span> /10
+        </p>
+        <p class="describe">
+         {props.children}
+        </p>
+        <p class="run-time">
+          {" "}
+          Horario: {props.duracion}.<span>MMPA: PG-13 </span>.
+          <span>Fecha: {props.fecha}</span>
+        </p>
+        <p>
+          Director: <a href="#">{props.director}</a>
+        </p>
+        <p>
+          Actores: <a href="#">{props.actores}</a>
+        </p>
+      </div>
+    </div>
+  );
+}
